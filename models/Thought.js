b1648b6fbffd1670dfe579ba7127b496a8d87538
CameronGillum <GillumTCameron@gmail.com>
2024-08-08 11:19:27 -0400

    finished thought model

--- models/Thought.js
+++ models/Thought.js
@@ -9,7 +9,7 @@ const reactionSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    createdAt: {
+    timestamp: {
         type: Date,
         default: Date.now
     }
@@ -31,9 +31,39 @@ const thoughtSchema = new mongoose.Schema({
     reactions: [reactionSchema]
 });
 
+// Virtual for reaction count
 thoughtSchema.virtual('reactionCount').get(function() {
     return this.reactions.length;
 });
+
+// Method to format timestamp
+thoughtSchema.methods.formatTimestamp = function(timestamp) {
+    return timestamp.toLocaleString('en-US', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true
+    }).replace(/, (\d{4}),/, ' $1 at');
+};
+
+// Override toJSON method to include formatted timestamp for thought and reactions
+thoughtSchema.methods.toJSON = function() {
+    const obj = this.toObject();
+    obj.formattedTimestamp = this.formatTimestamp(this.timestamp);
+
+    if (obj.reactions) {
+        obj.reactions = obj.reactions.map(reaction => {
+            reaction.formattedTimestamp = this.formatTimestamp(reaction.timestamp);
+            return reaction;
+        });
+    }
+
+    obj.reactionCount = this.reactionCount; // Include reaction count
+
+    return obj;
+};
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
